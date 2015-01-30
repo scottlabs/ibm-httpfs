827ac5021146c79ca9b8ab1a2e60bab1e76015b0
Kevin Scott <kscott@tenable.com>
2015-01-30 14:22:10 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ibm-hdfs
+# Description
+
+This exposes an API for interacting with IBM's HDFS (Hadoop Distributed File System). In particular, it exposes a simple request function upon which additional routes can be created.
+
+I've only written functions for listing directories and uploading files. However, full documentation on IBM's HDFS [can be found here](http://www-01.ibm.com/support/knowledgecenter/SSPT3X_3.0.0/com.ibm.swg.im.infosphere.biginsights.admin.doc/doc/admin_fileupload_rest_apis.html) if you want to implement other routes.
+
+# Install
+
+    npm install ibm-hdfs
+
+# Use
+
+```
+var HDFS = require('ibm-hdfs');
+
+var hdfs = new HDFS({
+    user: 'foo',
+    password: 'bar',
+    url: 'https://server:port/webhdfs/v1/'
+});
+
+console.log(hdfs.listDirectory('/dir'));
--- index.js
+++ index.js
@@ -1,7 +1,79 @@
-var makeRequest = require('./makeRequest');
+var request = require('request').defaults({ jar: true });
+var Q = require('q');
 
-var WebHDFS = function(params) {
-    //user, password, url
+var jar;
+var jarSaved;
+var jarExpiration = 10 * 60 * 60 * 1000; // 10 hours
+
+var HDFS = function(params) {
+    // parse incoming URL, we'll need the server.
+    var server = params.url.split('://').pop().split(':').shift();
+
+    function getCookie() {
+        var dfd = Q.defer();
+
+        var port = 8443;
+        var path = '/j_security_check';
+
+        var url = 'https://' + server + ':' + port + path;
+
+        var j = request.jar();
+
+        request.post({
+            url: url,
+            form: {
+                j_username: config.user,
+                j_password: config.password,
+            },
+            jar: j
+        }, function (error, response, body) {
+            if ( error ) {
+                dfd.reject(error);
+            } else {
+                dfd.resolve(j);
+            }
+        });
+
+        return dfd.promise;
+    };
+
+    function getToken() {
+        var dfd = Q.defer();
+        if ( ! jar || jarSaved < (new Date()).getTime() - jarExpiration ) {
+            getCookie().then(function(newJar) {
+                jar = newJar;
+                jarSaved = (new Date()).getTime();
+                dfd.resolve(jar);
+            });
+        } else {
+            dfd.resolve(jar);
+        }
+        return dfd.promise;
+    };
+    function makeRequest(query) {
+        var dfd = Q.defer();
+        var port = 14443;
+        var path = '/webhdfs/v1/';
+
+        var url = 'https://' + server + ':' + port + path + query;
+
+        getToken().then(function(j) {
+            request.get({
+                url: url,
+                jar: j
+            }, function (error, response, body) {
+                if ( error ) {
+                    dfd.reject(error);
+                } else {
+                    dfd.resolve(body);
+                }
+            });
+        });
+
+        return dfd.promise;
+    };
+
+    // Exposed functions
 
     function listDirectory(dir) {
         return makeRequest(dir+'?op=LISTSTATUS');
@@ -19,4 +91,4 @@ var WebHDFS = function(params) {
     };
 }();
 
-module.exports = WebHDFS;
+module.exports = HDFS;
